9f933a7d7d96e149f884433313f3eed750ee2568
thelastgoth128 <thindwacyrus86@gmail.com>
2026-05-23 23:39:54 +0200

    user input

---
+++ UserInput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int x, y;
+
+    cout << "Enter two numbers you want to sum." << endl;
+    cin >> x >> y;
+    cout << "The sum of " << x << " and " << y << " is " << x + y << endl;
+
+    return 0;
+}
