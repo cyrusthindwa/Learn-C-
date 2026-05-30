e6d3f3e4d592cd91e62b4f32afe8cf9fc1911fb0
thelastgoth128 <thindwacyrus86@gmail.com>
2026-05-30 23:32:57 +0200

    string functions

---
+++ StringFunctions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main () {
+    char str1[10] = "Hello";
+    char str2[10] = "World";
+    char str3[10];
+    int len;
+
+    // copy str1 to str3
+    strcpy(str3, str1);
+
+    cout << "str3: " << str3 << endl;
+
+    // concatenate str1 and str2
+    strcat(str1, str2);
+    cout << "str1: " << str1 << endl;
+
+    // total length of str1 after concatenation
+    len = strlen(str1);
+    cout << "Length of str1: " << len << endl;
+
+    return 0;
+}
